3bbacb7858efa174a310ff53c7fc1e710c6735cb
Arthur Follador <arthurfollador99@gmail.com>
2025-09-08 16:54:28 -0300

    Criando servidor Express

--- NodeJs/index.js
+++ NodeJs/index.js
@@ -2,9 +2,9 @@ const { Person } = require("./person");
 
 // require("./modules/path")
 // require("./modules/fs");
+// require("./modules/http");
+require("./modules/express.js");
 
-require("./modules/http");
-
-// const person = new Person("Felipe");
+const person = new Person("Felipe");
 
 // console.log(person.sayMyName());
